1f1bea273426bde77436a00d23501a906c066f5e
kalyan-git-repo <kalyancreamcollar@gmail.com>
2025-09-27 19:41:26 +0530

    30 days workout plan

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Grid from "./components/Grid"
+import Hero from "./components/Hero"
+import Layout from "./components/Layout"
+export default function App() {
+  return (
+    <Layout>
+      <main>
+        <Hero />
+        <Grid />
+      </main>
+    </Layout>
+  )
+}
+
